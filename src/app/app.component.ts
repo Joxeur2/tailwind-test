c4b41b92a72aba6015392e417ca166fe78dfefc5
Jonas Lauber <jonas.lauber@aspect3.ch>
2025-02-05 07:31:45 +0100

    Removed code useless for bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {TestComponent} from './component/test.component';
 
 @Component({
   selector: 'app-root',
-  imports: [TestComponent],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'test-project';
 }
